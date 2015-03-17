09f6567c06a09e3702af241c8bc4755f4fdf867c
JonathanStarnes <jonathanstarnes@gmail.com>
2015-03-17 07:22:35 -0500

    added melpa

--- init.el
+++ init.el
@@ -6,10 +6,19 @@
 ;; No splash screen please ... jeez
 (setq inhibit-startup-message t)
 
+;; Melpa
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
+
+
 ;; Add all of the folders from the plugins directory
 (let ((default-directory "~/.emacs.d/plugins/"))
   (normal-top-level-add-subdirs-to-load-path))
-
 
 ;; csharp-mode
 (require 'csharp-mode)
@@ -24,3 +33,20 @@
 ;; company mode
 (require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
+
+;; omnisharp
+(add-hook 'csharp-mode-hook 'omnisharp-mode)
+(eval-after-load 'company
+  '(add-to-list 'company-backends 'company-omnisharp))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("c:/test/test.org"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
