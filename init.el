68089e430901bec6f9ceddbf04fc53b19d6d2eb6
JonathanStarnes <jonathanstarnes@gmail.com>
2015-03-29 22:54:40 -0500

    Added themes

--- init.el
+++ init.el
@@ -49,11 +49,19 @@
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb) 
 
+;; theme
+(require 'emacs-color-themes)
+
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("77d05e0aa0af3321a18aef1a9cea1d12ee0cbc1cfee4c0a1612813469f89e721" "ec0f5324cdd147558e44c5ae8c25f6709400bda26280be9bf9474e73ebe36afe" "8da4938e0e5754d199ef23087edbddfadf78ecbacbd49b6c766d64296310e3e3" "35ce30aa61c3d288dfb6f0687420d8773c6281e77cf07dc9dc9e9e9c315d29ae" default)))
  '(org-agenda-files (quote ("c:/test/test.org"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
